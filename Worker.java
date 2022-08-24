6dbc4a528119e809f6bf7899e7fcb9f9f98493bb
Ekaterina Zhitkevich <zhitkevich.ekaterina@yandex.ru>
2022-08-24 16:41:22 +0300

    Add OnTaskErrorListener

--- Worker.java
+++ Worker.java
@@ -2,14 +2,19 @@ package worker;
 
 public class Worker {
     private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
 
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
     }
 
     public void start() {
         for (int i = 0; i < 100; i++) {
-            callback.onDone(i + ". Тик-так");
+            callback.onDone("Задача № " + i + " успешно выполнена ");
+            if (i == 33){
+                errorCallback.onError("Неизвестная ошибка на "+ i + " задаче!");
+            }
         }
     }
 }
